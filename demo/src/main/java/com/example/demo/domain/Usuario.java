cb78018ec3056c098a5902db4e32085b1f7d2826
ArgosKnight <cardenasdaniel82@gmail.com>
2021-05-28 13:04:50 -0500

    Cuarto commit

--- demo/src/main/java/com/example/demo/domain/Usuario.java
+++ demo/src/main/java/com/example/demo/domain/Usuario.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,7 +13,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import antlr.collections.List;
 
 @Entity
 @Table(name="usuario")
